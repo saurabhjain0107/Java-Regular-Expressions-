35d05a93582e64a3e0e8ae3e49bd0c53e721f202
saurabh jain <luckyjain0107@gmai.com>
2022-09-05 15:46:32 +0530

    User First Name

--- Regex.java
+++ Regex.java
@@ -1,7 +1,28 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Regex {
+
+    public void isFirstName() {
+        Pattern pattern = Pattern.compile("^[A-Z][a-z A-Z]{2,}");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first name :");
+        String FirstName = sc.nextLine();
+        Matcher matcher = pattern.matcher(FirstName);
+        if (matcher.matches()) {
+            System.out.println("First name is valid : ");
+        }
+        else {
+            System.out.println("First name is Invalid : ");
+
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to Regex");
+        Regex regex = new Regex();
+        regex.isFirstName();
+
     }
 }
